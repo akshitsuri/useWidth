30b7f4abdf9d814b2ed60e67d3b15725445aaa5a
Akshit Suri <a.akshitsuri@gmail.com>
2021-09-05 02:16:08 +0530

    minor changes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 const App = () => {
-  return <div>hey</div>;
+  return (
+    <div>
+      NPM package to use width of screen size dynamically for responsive design
+    </div>
+  );
 };
 
 export default App;
